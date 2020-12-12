19662a38016e0cae5f4e77d4680d8ef2e038c142
Ferry Kurniawan <ferryku8@gmail.com>
2020-12-12 19:21:12 +0700

    TUGAS 6 C++
    Mengumpulkan tugas 6 C++

---
+++ tugas 6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int x,z;7 
+	cout<<"Inputkan Nilai x : ";
+	cin>>x;
+	cout<<"Inputkan Nilai z : ";
+	cin>>z;
+	if(x>z){
+		cout<<"Hasil : "<<x+z;
+	}else {
+		cout<<"Hasil : "<<x-z;
+	}
+}
